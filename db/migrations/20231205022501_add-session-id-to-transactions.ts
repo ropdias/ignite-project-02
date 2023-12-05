1a3d6d66c3e52168042a6107eaef3e2caaf32d0c
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-12-04 23:32:08 -0300

    creating a database using knex

---
+++ db/migrations/20231205022501_add-session-id-to-transactions.ts
@@ -0,0 +1,16 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    //some dbs support .after() and some dont
+    //index() to create and index because we will use a lot the session_id
+    //so we can create a cache of it to load faster
+    table.uuid('session_id').after('id').index();
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('session_id');
+  });
+}
