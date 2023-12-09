63c0a5856875863b0e065028a6281f2bf4330a95
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-12-09 00:21:30 -0300

    adding zod, dotenv and using knex

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,22 @@
 import fastify from 'fastify';
 import { knex } from './database';
+import { env } from './env';
 
 const app = fastify();
 
 // GET, POST, PUT, PATCH, DELETE
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*');
 
-  return tables;
+  return transaction;
 });
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!');
