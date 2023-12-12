2a29152949b3fb2c63bfcc976591b788675a6a24
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-12-11 23:48:08 -0300

    configuring a test database changing the .env to .env.test and executing migrations beforeEach() test using the execSync() to reset the test database

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,5 @@
-import { expect, it, beforeAll, afterAll, describe } from 'vitest';
+import { expect, it, beforeAll, afterAll, describe, beforeEach } from 'vitest';
+import { execSync } from 'node:child_process';
 import request from 'supertest';
 import { app } from '../src/app';
 
@@ -11,6 +12,11 @@ describe('Transactions routes', () => {
   afterAll(async () => {
     // Wait fastify to close the server
     await app.close();
+  });
+
+  beforeEach(() => {
+    execSync('npm run knex -- migrate:rollback --all');
+    execSync('npm run knex -- migrate:latest');
   });
 
   it('should be able to create a new transaction', async () => {
